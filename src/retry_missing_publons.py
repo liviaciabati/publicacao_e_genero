19ae475258b86d58acec317bebd6470bf3d298dc
Ariane Morassi Sasso <Ariane.Morassi-Sasso@hpi.de>
2020-06-13 17:01:23 +0000

    Simplicado o processo

--- src/retry_missing_publons.py
+++ src/retry_missing_publons.py
@@ -12,6 +12,8 @@ Updated on Apr 2020
 import json
 from os.path import join, exists
 
+from general import get_files
+
 def main():
     print('Iniciando...')
 
@@ -22,42 +24,26 @@ def main():
     if not exists(publons_data_path):
         makedirs(publons_data_path)
 
-    # Procura dados analisados previamente
-    data_analysed_file = join(publons_data_path, 'data_analysed.txt')
-    if exists(data_analysed_file):
-        with open(data_analysed_file, 'r') as f:
-            data_analysed = f.read().split(',')
-    else:
-        data_analysed = []
-    print('Qtd. de dados analisados previamente: ', len(data_analysed))
-
-    # Procura dados analisados previamente com problema
-    data_missing_file = join(publons_data_path, 'data_missing.txt')
-    if exists(data_missing_file):
-        with open(data_missing_file, 'r') as f:
-            data_missing = f.read().split(',')
-        if len(data_missing) == 1 and data_missing.pop() == '':
-            print('Sem dados faltantes.')
-            return 0
-    else:
-        print('Sem dados faltantes.')
-        return 0
-    print('Qtd. de dados com problema previamente: ', len(data_missing))
-
-    for missing in data_missing:
-        if missing in data_analysed:
-            data_analysed.remove(missing)
+    # Procura dados recuperados previamente
+    data_recovered_files = get_files(publons_data_path, 'json')
+    data_recovered = []
+    if data_recovered_files:
+        for data_recovered_file in data_recovered_files:
+            usp_id = data_recovered_file.split('_')[0]
+            data_recovered.append(usp_id)
+    print('Qtd. de dados recuperados previamente: ', len(data_recovered))
         
-    # Escrevendo arquivo com dado recuperado e excluindo missing
-    data_analysed.reverse() # coloca os últimos arquivos primeiro
+    # Escrevendo arquivo com dados recuperados
+    data_analysed_file = join(publons_data_path, 'data_analysed.txt')
     with open(data_analysed_file, 'w', newline='') as f:
-        f.write(','.join(set(data_analysed)))
+        f.write(','.join(set(data_recovered)))
 
     # Deletando arquivo de dados faltantes
+    data_missing_file = join(publons_data_path, 'data_missing.txt')
     with open(data_missing_file, 'w', newline='') as f:
         f.write('')
     
-    print('Qtd. de dados no novo arquivo: ', len(data_analysed))
+    print('Qtd. de dados no novo arquivo: ', len(data_recovered))
 
 if __name__ == '__main__':
     main()
